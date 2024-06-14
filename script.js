e6c90198f30c9a54001ba6f1fc8066718cfc172f
silverprimez <124439185+silverprimez@users.noreply.github.com>
2024-06-14 15:42:06 +0800

    v1

--- script.js
+++
@@ -1,8 +0,0 @@
-//turn pages when click next or prev button
-//contact me button when click
-//create reverse index function
-//back profile button when click
-//opening animation
-//opening animation (cover right animation)
-//opening animation (page left or profile page animation)
-//opening animation (all page right animation)
